160d9773f9e3397d72a7fceb5cff2da0da0e7640
Ritika999 <70030520+Ritika999@users.noreply.github.com>
2021-07-25 09:43:56 +0530

    Update SearchScreen.js

--- SearchScreen.js
+++ SearchScreen.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { Text, View, FlatList, StyleSheet, TextInput, TouchableOpacity} from 'react-native';
 import db from '../config'
-//import { ScrollView } from 'react-native-gesture-handler';
-
-
 
 export default class Searchscreen extends React.Component {
   
@@ -16,56 +13,7 @@ export default class Searchscreen extends React.Component {
       }
     }
 
-    //remove
-    fetchMoreTransactions = async ()=>{
-      var text = this.state.search.toUpperCase()
-      var enteredText = text.split("")
-
-      
-      if (enteredText[0].toUpperCase() ==='B'){
-      const query = await db.collection("transactions").where('bookId','==',text).startAfter(this.state.lastVisibleTransaction).limit(10).get()
-      query.docs.map((doc)=>{
-        this.setState({
-          allTransactions: [...this.state.allTransactions, doc.data()],
-          lastVisibleTransaction: doc
-        })
-      })
-    }
-      else if(enteredText[0].toUpperCase() === 'S'){
-        const query = await db.collection("transactions").where('bookId','==',text).startAfter(this.state.lastVisibleTransaction).limit(10).get()
-        query.docs.map((doc)=>{
-          this.setState({
-            allTransactions: [...this.state.allTransactions, doc.data()],
-            lastVisibleTransaction: doc
-          })
-        })
-      }
-  }
-
-  //remove
-    searchTransactions= async(text) =>{
-      var enteredText = text.split("") 
-
-      if (enteredText[0].toUpperCase() ==='B'){
-        const transaction =  await db.collection("transactions").where('bookId','==',text).get()
-        transaction.docs.map((doc)=>{
-          this.setState({
-            allTransactions:[...this.state.allTransactions,doc.data()],
-            lastVisibleTransaction: doc
-          })
-        })
-      }
-      else if(enteredText[0].toUpperCase() === 'S'){
-        const transaction = await db.collection('transactions').where('studentId','==',text).get()
-        transaction.docs.map((doc)=>{
-          this.setState({
-            allTransactions:[...this.state.allTransactions,doc.data()],
-            lastVisibleTransaction: doc
-          })
-        })
-      }
-    }
-
+   
 
     componentDidMount = async ()=>{
       const query = await db.collection("transactions").limit(10).get()
@@ -89,26 +37,10 @@ export default class Searchscreen extends React.Component {
 
             <TouchableOpacity
               style = {styles.searchButton}
-              onPress={()=>{this.searchTransactions(this.state.search)}}
             >
               <Text>Search</Text>
             </TouchableOpacity>
           </View>
-
-        <FlatList
-          data={this.state.allTransactions}
-          renderItem={({item})=>(
-            <View style={{borderBottomWidth: 2}}>
-              <Text>{"Book Id: " + item.bookId}</Text>
-              <Text>{"Student id: " + item.studentId}</Text>
-              <Text>{"Transaction Type: " + item.transactionType}</Text>
-              <Text>{"Date: " + item.date.toDate()}</Text>
-            </View>
-          )}
-          keyExtractor= {(item, index)=> index.toString()}
-          onEndReached ={this.fetchMoreTransactions}
-          onEndReachedThreshold={0.7}
-        /> 
 
         </View>
       );
